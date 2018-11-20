4c62c6191c4e4f27150f4236b0af74c0c18118bc
macchiatoyn <shinyyn@gmail.com>
2018-11-19 19:25:06 -0600

    Update Wedding_Album.py

--- Wedding_Album.py
+++ Wedding_Album.py
@@ -13,17 +13,17 @@ import requests
 #  tylerandnataliesayido
 
 quince_dict = {
-    'dress':'prep',
+    'dress': 'prep',
     'hair': 'prep',
     'excite': 'prep',
     'makeup': 'prep',
     'make up': 'prep',
-    'venue': 'prep',
     'escort': 'prep',
     'ready': 'prep',
     'florals': 'prep',
-    'flower' :'prep',
+    'flower': 'prep',
     'table': 'prep',
+    'celebrat': 'prep',
     'silverware': 'prep',
     'placesetting': 'prep',
     'decor': 'prep',
@@ -31,8 +31,6 @@ quince_dict = {
     'arrange': 'prep',
     'design': 'prep',
     'entrance': 'prep',
-    'dance': 'dance',
-    'dancing': 'dance',
     'music': 'dance',
     'waltz': 'dance',
     'father daughter': 'dance',
@@ -53,34 +51,33 @@ quince_dict = {
     'cake': 'food',
     'bake': 'food',
     'baked': 'food',
-    'candy': 'food',
     'dessert': 'food',
     'cut': 'food',
     'tier': 'food',
     'yum': 'food',
     'food': 'food',
-    'bar':'food',
-    'chocolate':'food',
-    'fruit':'food',
-    'drink':'food',
-    'cheers':'food',
+    'bar': 'food',
+    'chocolate': 'food',
+    'fruit': 'food',
+    'drink': 'food',
+    'cheers': 'food',
     'delicious': 'food',
     'catering': 'food',
     'sugar': 'food',
     'sweet': 'food',
     'confection': 'food',
     'photography': 'photoshoot',
-    'photographer':'photoshoot',
+    'photographer': 'photoshoot',
     'pic': 'photoshoot',
     'shot': 'photoshoot',
     'model': 'photoshoot',
-    'headshot':'photoshoot',
+    'headshot': 'photoshoot',
     'shoot': 'photoshoot',
     'booth': 'photobooth',
-    
-    
-    
 }
+
+#result_temp = {"qui_prep":set(qui_prep),"recessional":set(recessional),"dance":set(dance),"cake": set(cake),
+#                   "dinner": set(dinner), "what also happened": set(other)}
 wedding_dict = {
     'wedding dress': 'wedding prep',
     'gown': 'wedding prep',
@@ -153,7 +150,7 @@ wedding_dict = {
 
 
 # returns photos prioritizing covering categories and then likes
-def return_photos_wedding(result_temp):
+def return_photos(result_temp):
     count = 0
     wedding_prep = []
     prelude = []
@@ -199,34 +196,41 @@ def return_photos_wedding(result_temp):
 
     return result
 
+# returns photos prioritizing covering categories and then likes
 def return_photos_quince(result_temp):
     count = 0
     prep = []
     dance = []
     food = []
-    photoshoot = []
     photobooth = []
+    photoshoot = []
+    other = []
+    seen = []
+
     while count < 12:
         for set_name, photo_set in result_temp.items():
             if photo_set != set([]) and count < 12:
                 photo, seen = best_photo(photo_set, seen)
+
                 if len(photo) != 0:
                     count = count + 1
                     if set_name == "food":
                         food.append(photo)
-                    elif set_name == "prep":
-                        prep.append(photo)
                     elif set_name == "dance":
                         dance.append(photo)
-                    elif set_name == "photshoot":
-                        photoshoot.append(photo)
+                    elif set_name == "qui_prep":
+                        prep.append(photo)
                     elif set_name == "photobooth":
                         photobooth.append(photo)
+                    elif set_name == "photoshoot":
+                        photoshoot.append(photo)
+                    elif set_name == "what also happened":
+                        other.append(photo)
                     seen.append(photo)
 
 
-    result = {"prep":prep, "dance":dance, "food":food, "photoshoot":photoshoot,
-         "photobooth":photobooth}
+    result = {"qui_prep":prep, "photoshoot":photoshoot, "dance":dance, "food":food, "photobooth":photobooth,
+              "what also happened":other}
 
     return result
 
@@ -320,23 +324,96 @@ def extract_wedding_images(soup):
               "recessional":set(recessional), "reception":set(reception),"first dance":set(first_dance), "cake": set(cake),
               "what also happened": set(other)}
 
-    result = return_photos_wedding(result_temp)
+    result = return_photos(result_temp)
+
+    return result
+
+
+# Extract and return images based on keywords
+# Input: soup
+# Output: a dict of links with subevents
+def extract_quince_images(soup):
+    scripts = soup.find_all('script')
+    all_info = scripts[3].text.strip()
+
+    prep = []
+    photoshoot = []
+    dance = []
+    food = []
+    photobooth = []
+    other = []
+
+
+    nodes = all_info.split("\"node\":")
+    for x in nodes:
+        if "text" in x and "display_url" in x:
+            text = x.split('},')[0].lower()
+            shortcode = x.split('"shortcode":"')[1].split('",')[0]
+            username = "@" + extract_username(shortcode)
+
+            liked_count = x.split('"edge_liked_by":{"count":')[1].split('}')[0]
+            jpg_link = x.split('"display_url":"')[1].split('"')[0]
+            flag = 0
+
+            # Search for a keyword in "text", and save corresponding url
+            for keyword in wedding_dict.keys():
+                if keyword in text:
+                    if wedding_dict.get(keyword) == "food":
+                        food.append((jpg_link,liked_count,username))
+                        flag = 1
+                    elif wedding_dict.get(keyword) == "dance":
+                        dance.append((jpg_link,liked_count,username))
+                        flag = 1
+                    elif wedding_dict.get(keyword) == "prep":
+                        prep.append((jpg_link,liked_count,username))
+                        flag = 1
+                    elif wedding_dict.get(keyword) == "photoshoot":
+                        photoshoot.append((jpg_link,liked_count,username))
+                        flag = 1
+                    elif wedding_dict.get(keyword) == "photobooth":
+                        photobooth.append((jpg_link,liked_count,username))
+                        flag = 1
+            if not flag == 1:
+                other.append((jpg_link,liked_count,username))
+
+
+    result_temp = {"qui_prep":set(prep),"photoshoot":set(photoshoot),"dance":set(dance),"food": set(food),
+                   "photobooth": set(photobooth), "what also happened": set(other)}
+
+
+    for k,v in result_temp.items():
+        print(k)
+        print(v)
+
+    result = return_photos_quince(result_temp)
 
     return result
 
 
 if __name__ == '__main__':
-    tag = input("Please enter a hashtag of your event: ")
+    #event_type = input("Please enter the type of your event: ")
+    event_type = "quince"
+
+    #tag = input("Please enter a hashtag of your event: ")
+    tag = "elenasquinceparty"
     url = "https://www.instagram.com/explore/tags/" + tag + "/"
+
 
     r = requests.get(url)
     page = r.text
     soup = BeautifulSoup(page, 'html.parser')
 
-    photos = extract_wedding_images(soup)
+    if event_type == "wedding":
+        photos = extract_wedding_images(soup)
+    elif event_type == "quince":
+        photos = extract_quince_images(soup)
+    else:
+        print("Error! Check your input!")
 
-    print("Links of wedding images: ")
+
+    print("Links of images: ")
     for k,v in photos.items():
         print(k)
-        print(v)
+        for x in v:
+            print(x)
         print()
